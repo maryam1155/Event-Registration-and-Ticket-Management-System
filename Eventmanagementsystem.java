85938deeb3298a678b7fd2a72fcfdaff3cfbbb4e
maryam1155 <maryam115567@gmail.com>
2026-01-06 20:44:32 +0300

    Add files via upload

---
+++ Eventmanagementsystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eventmanagementsystem;
+
+/**
+ *
+ * @author marya
+ */
+public class Eventmanagementsystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
